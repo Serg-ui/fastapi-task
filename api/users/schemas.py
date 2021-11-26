4d751cc3fea38698f6a448248d78ded0d6682773
Serg <serrg.y85@gmail.com>
2021-11-26 18:19:14 +0300

    email validation

--- api/users/schemas.py
+++ api/users/schemas.py
@@ -20,6 +20,13 @@ class CreateUserScheme(BaseModel):
             raise ValueError('Пароль должен быть не менее 6 символов и содержать хотя бы 1 букву и одну цифру')
         return value
 
+    @validator('e_mail')
+    def email_valid(cls, value):
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if re.match(pattern, value) is None:
+            raise ValueError('Не валидный email')
+        return value
+
 
 class UpdateUserScheme(CreateUserScheme):
     user_id: int
